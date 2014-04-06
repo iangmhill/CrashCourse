8d9badc802b06482cfa2805dd0b2f2a506591424
Susie Grimshaw <susan.grimshaw@students.olin.edu>
2014-04-06 17:35:55 -0400

    Dash?

---
+++ Kivy_Explore/Dash/dash1.py
@@ -0,0 +1,49 @@
+from kivy.app import App
+from kivy.uix.tabbedpanel import TabbedPanel
+from kivy.uix.floatlayout import FloatLayout
+from kivy.lang import Builder
+from kivy.uix.checkbox import CheckBox
+from kivy.uix.dropdown import DropDown
+from kivy.uix.button import Button
+from kivy.base import runTouchApp
+from kivy.uix.slider import Slider
+from kivy.uix.spinner import Spinner
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+
+kv = '''
+GridLayout:
+    cols:2
+    rows:2
+    canvas:
+        Color:
+            rgba: 0,1,1,.5
+        Rectangle:
+            pos: self.pos
+            size: self.size
+    Label:
+        text: 'Your Information'
+    Label:
+        text: 'Reminders'
+    Label:
+        text: 'Statistics'
+    Label:
+        text: 'Notes'
+        TextInput:
+            size_hint: (.25,.5)
+            pos_hint: {'x':.7,'y':.3}
+            pos:(600,20)                
+            text: 'Enter Notes Here'  
+    
+'''
+
+
+class MyPaintApp(App):
+    def build(self):
+        return Builder.load_string(kv)
+
+    def greet(self):
+        pass
+
+if __name__ == '__main__':
+    MyPaintApp().run()
