669de743319e89df5c189fc1ac624179074f59cd
Susie Grimshaw <susan.grimshaw@students.olin.edu>
2014-04-11 17:46:13 -0400

    And now commented :D

--- Kivy_Explore/Login/LogIn3.py
+++ Kivy_Explore/Login/LogIn3.py
@@ -1,28 +1,18 @@
-import datetime
 from kivy.app import App
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.properties import ObjectProperty
-from kivy.properties import StringProperty
-from sleekxmpp import ClientXMPP
-from sleekxmpp.exceptions import XMPPError
-from sleekxmpp.jid import InvalidJID
-from kivy.uix.textinput import TextInput
-from kivy.uix.modalview import ModalView
-from kivy.uix.label import Label
-from kivy.uix.button import Button
-from kivy.uix.listview import ListItemButton
-from kivy.uix.boxlayout import BoxLayout
-from kivy.utils import escape_markup
-from kivy.core.audio import SoundLoader
 
+
+
+#I commented out the server things cause I wasn't sure if we needed them or not. I also commented it out in the orkiv.kv file. 
 
 class AccountDetailsForm(AnchorLayout):
-    server_box = ObjectProperty()
+    #server_box = ObjectProperty()
     username_box = ObjectProperty()
     password_box = ObjectProperty()
     def login(self):
-        print ("Click the Button")
-        print(self.server_box.text)
+        #Use the things in the print statements below to access the information in the server, username, and password boxes after login is pressed
+        #print(self.server_box.text)
         print(self.username_box.text)
         print(self.password_box.text)
 
